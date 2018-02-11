46db9678468edd1555e22491f2852ea9005b45b3
2apl <ruslan_apl@mail.ru>
2018-02-11 08:21:58 +0300

    Task1 reload

--- Lesson10_HomeWork/Task1/Text.java
+++ Lesson10_HomeWork/Task1/Text.java
@@ -4,6 +4,6 @@ public class Text {
 
     public static void main(String[] args) {
         String text = "Hi. I am a programmer on java language. Java is the best language for a programmer.";
-        System.out.println(TextWord.verbsInText(text));
+        System.out.println(TextWord.wordsInText(text));
     }
 }
